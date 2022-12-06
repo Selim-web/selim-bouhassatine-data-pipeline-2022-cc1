7a2a7fb089fd866068d08f72336fbf67f8cc93ee
Selim <selim.bouhassatine@etu.u-pec.fr>
2022-12-06 17:13:04 +0100

    first commit

---
+++ question 6/terraform/main.tf
@@ -0,0 +1,22 @@
+
+# Définissez les variables d'entrée
+variable "stream_name" {
+	type = string
+}
+
+variable "partition_count" {
+  type = number
+  default = 1
+}
+
+# Définissez le type de ressource à créer
+resource "aws_kinesis_stream" "selim-bouhassatine-stock-input-stream" {
+  name           = var.stream_name
+  shard_count    = var.partition_count
+  tags = {
+	Name = "selim.bouhassatine@etu.u-pec.fr"
+	Owner = "selim_dev"
+  }
+}
+
+
